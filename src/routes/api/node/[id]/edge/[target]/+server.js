0f1c3b01d52417929218d9b67869b190243aaaf2
JCY <chery.jerome@gmail.com>
2023-04-03 14:04:39 +0200

    Update Edge

--- src/routes/api/node/[id]/edge/[target]/+server.js
+++ src/routes/api/node/[id]/edge/[target]/+server.js
@@ -1,11 +1,21 @@
-import { findEdge } from '$lib/database';
+import { findEdge, insertEdge } from '$lib/database';
 import { json } from '@sveltejs/kit';
 
 export const GET = (({ params }) => {
 	const id = params.id;
-    const target = params.target;
+	const target = params.target;
 
 	const edges = findEdge(id, target) ?? [];
 
 	return json(edges);
 })
+
+export const PUT = async ({ params, request }) => {
+	const id = params.id;
+	const target = params.target;
+	const node = await request.json();
+
+	const resu = insertEdge(id, target, JSON.stringify(node));
+
+	return json(resu);
+}
